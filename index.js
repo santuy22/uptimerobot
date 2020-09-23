0ad7c197e4435807bfd0f4f41e5f64c9541d65de
santuy22 <71236272+santuy22@users.noreply.github.com>
2020-09-23 11:14:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://balsam-spark-show.glitch.me"]
+const urls = ["https://learned-seemly-twister.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
