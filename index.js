c9b5e0a79d9989c53b60380dd48637ad52b54af2
santuy22 <71236272+santuy22@users.noreply.github.com>
2020-09-24 18:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://inconclusive-incongruous-boysenberry.glitch.me"]
+const urls = ["https://titanium-abrasive-snowflake.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
