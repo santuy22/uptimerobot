25e9e4118f744b0bf8f61a47f99e4425ab7e31d4
santuy22 <71236272+santuy22@users.noreply.github.com>
2020-10-07 22:49:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://largwa-music-bot.glitch.me"]
+const urls = ["https://lyrick-homi.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
